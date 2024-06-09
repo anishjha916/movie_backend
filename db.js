e0bbdd2a733dc55f5c5d6e60adac4f0d5b76debd
Anish jha <anishjha805@gmail.com>
2024-06-09 10:49:58 +0530

    Add files via upload

---
+++ db.js
@@ -0,0 +1,12 @@
+const  mongoose  = require('mongoose');
+require('dotenv').config(); 
+
+ 
+mongoose.connect(process.env.MONGO_URL,{
+    dbName:process.env.DB_NAME
+}).then(() => {
+    console.log("connected to the database");
+}   
+).catch((err) => {
+    console.log('error database' +err);  
+})  
